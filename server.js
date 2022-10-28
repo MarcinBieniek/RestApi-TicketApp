1adba1176e2f193c574619aec128073a5273cb9f
Marcin Bieniek <marcingim@gmail.com>
2022-10-28 13:07:35 +0200

    Create MDB Atlas data base and connect to project

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use((req, res) => {
 })
 
 // connects our backend code with the database
-mongoose.connect('mongodb://localhost:27017/NewWaveDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://MarcinEden:MarciNgim@cluster0.tgryn8j.mongodb.net/NewWaveDB');
 const db = mongoose.connection;
 
 db.once('open', () => {
